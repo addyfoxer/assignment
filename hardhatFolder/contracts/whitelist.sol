b2cb5d15305112a27ff3371308cfc2a3519b86a9
addyfoxer <adarshku12345@gmail.com>
2023-07-18 23:22:26 +0530

    changes

--- hardhatFolder/contracts/whitelist.sol
+++ hardhatFolder/contracts/whitelist.sol
@@ -6,14 +6,13 @@ contract Whitelist {
     uint8 public maxWhitelistedAddresses;
 
     // Create a mapping of whitelistedAddresses
-    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
     mapping(address => bool) public whitelistedAddresses;
 
-    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // numAddressesWhitelisted to keep track of how many addresses have been whitelisted
     uint8 public numAddressesWhitelisted;
 
     // Setting the Max number of whitelisted addresses
-    // User will put the value at the time of deployment
+   
     constructor(uint8 _maxWhitelistedAddresses) {
         maxWhitelistedAddresses =  _maxWhitelistedAddresses;
     }
